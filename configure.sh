eab205c7e1e56fc728a89e51ef116751fdd1e92e
Kevon Scott <kevon-dev@outlook.com>
2025-06-06 23:42:40 -0400

    Source control oh-my-posh config

--- configure.sh
+++ configure.sh
@@ -2,3 +2,6 @@
 
 echo "copying vim config to ~/.vimrc"
 cp $(dirname "$0")/.vimrc ~/.vimrc
+
+echo "copying oh-my-posh config to ~/.oh-my-posh-cfg"
+cp -r $(dirname "$0")/.oh-my-posh-cfg  ~/.oh-my-posh-cfg
